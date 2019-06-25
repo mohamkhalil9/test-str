1ce5f72087309542685e7710f204b2a372a4ed40
II_Snax_II <45282148+mohamkhalil9@users.noreply.github.com>
2019-06-25 23:47:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,54 +5,16 @@ var client = new Discord.Client();
 client.on('ready', () => {
   console.log(`Welcome Bro ${client.user.tag}!`);
 });
-var prefix = '^'
 
-client.on('message', message => {
-  if (!message.content.startsWith(prefix)) return;
-  var args = message.content.split(' ').slice(1);
-  var argresult = args.join(' ');
-  if (message.author.id !== "444596274189172758") return;
-
-  
-  if (message.content.startsWith(prefix + 'wat')) {
-  client.user.setActivity(argresult, {type: 'WATCHING'})
-     console.log('test' + argresult);
-      message.channel.send(`**Status You ${argresult} **`)
-} 
-
- 
-  if (message.content.startsWith(prefix + 'lis')) {
-  client.user.setActivity(argresult, {type: 'LISTENING'})
-     console.log('test' + argresult);
-      message.channel.send(`**Status You ${argresult} **`)
-} 
-
-
-if (message.content.startsWith(prefix + 'setname')) {
-  client.user.setUsername(argresult).then
-      message.channel.sendMessage(`Username Changed To **${argresult}**`)
-  return message.reply("You Can change the username 2 times per hour");
-} 
-
-if (message.content.startsWith(prefix + 'setavatar')) {
-  client.user.setAvatar(argresult);
-   message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
-}
-
-if (message.content.startsWith(prefix + 'st')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/peery13");
-     console.log('test' + argresult);
-      message.channel.send(`**Status You ${argresult} **`)
-} 
-if (message.content.startsWith(prefix + 'ply')) {
-  client.user.setGame(argresult);
-     console.log('test' + argresult);
-      message.channel.send(`**Status You ${argresult} **`)
-} 
-
-
-
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : EX Clan`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`أنـا آسـف ، ما كـان المفروض أسـوي اللي سويته …:broken_heart:`,"http://twitch.tv/Death Shop")
+client.user.setStatus("online")
 });
-
 
 client.login(process.env.BOT_TOKEN);// صنعت لدى محمد دوله
