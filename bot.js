0606f20a371b5ebfb236c070815fabf77fec8027
II_Snax_II <45282148+mohamkhalil9@users.noreply.github.com>
2019-06-25 23:39:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== "415142691282616330") return;
+  if (message.author.id !== "444596274189172758") return;
 
   
   if (message.content.startsWith(prefix + 'wat')) {
